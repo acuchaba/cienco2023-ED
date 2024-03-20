93b260671a353740572a4c8f4f7e00a15b23692f
Agnaldo Cuchaba <acuchaba@gmail.com>
2024-03-20 18:10:31 -0300

    Exercicos de Ponteiros

---
+++ Material2/Ponteiros/exec7.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+#define TAM 10
+
+int main()
+{
+    int vet[TAM];
+
+    for (int i = 0; i < TAM; i++)
+    {
+        cout << "Digite o " << i + 1 << "o valor inteiro: \n";
+        cin >> vet[i];
+    }
+
+    cout << "\nConteudo do vetor na ordem normal: \n";
+    int *ptr = vet;
+    for (int i = 0; i < TAM; i++)
+    {
+        cout << *ptr << " ";
+        ++ptr;
+    }
+
+    cout << endl;
+
+    cout << "\nConteudo do vetor na ordem inversa: \n";
+
+    ptr = vet + TAM - 1;
+
+    for (int i = 0; i < TAM; i++)
+    {
+        cout << *ptr << " ";
+        --ptr;
+    }
+
+    cout << endl;
+}
---
+++ Material2/Ponteiros/exec8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+#define TAM 5
+
+int main()
+{
+    int a[TAM];
+    int *ptr = a;
+
+    for(int i = 0; i < TAM; i++)
+    {
+        cout << "Insira o " << i + 1 << "o numero.\n";
+        cin >> *(ptr + i);
+    }
+
+    int soma = 0;
+
+    for(int i = 0; i < TAM; i++)
+    {
+        soma += *(ptr + i);
+    }
+
+    double media = (soma) / TAM;
+
+    cout << "A media dos numeros e: \n" << media;
+
+}
